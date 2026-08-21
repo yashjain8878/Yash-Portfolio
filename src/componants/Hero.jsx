c40799e371f138961852b26672071d1f37edb714
yashjain8878 <yashj8029@gmail.com>
2026-08-21 13:29:57 +0530

    new

--- src/componants/Hero.jsx
+++ src/componants/Hero.jsx
@@ -29,11 +29,11 @@ function Hero(){
             </a>
 
             <a href="https://github.com/yashjain8878" target="blank" className="w-9 h-9 rounded-full border-2 border-[#A3CEF1] flex items-center justify-center text-[#274C77] hover:bg-[#274C77] hover:text-white transition-all duration-300">
-              <img src="./public/githubIcon.png" alt="GitHub Icon" className="w-6 h-6" />
+              <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="GitHub Icon" className="w-6 h-6" />
             </a>
 
              <a href="https://www.linkedin.com/in/yash-jain-626837268/" target="blank" className="w-9 h-9 rounded-full border-2 border-[#A3CEF1] flex items-center justify-center text-[#274C77] hover:bg-[#274C77] hover:text-white transition-all duration-300">
-              <img src="./public/Linkdinicon.svg" alt="LinkedIn Icon" className="w-6 h-6" />
+              <img src="./public/LinkdinIcon.svg" alt="LinkedIn Icon" className="w-6 h-6" />
             </a>
 
           </div>
